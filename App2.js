8644f7c2841fd660ca293f8d851d21206ba93e6c
Ramirogt <91095533+Ramirogt@users.noreply.github.com>
2022-03-31 03:47:17 -0400

    Add files via upload

---
+++ App2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { YellowBox } from 'react-native';
+import EventList from './lib/listDate/EventList';
+
+import Logintest from './LoginScreen/Logintest';
+export default class App extends React.Component {
+  render() {
+    return (
+      <EventList />
+    );
+  }
+}
